7817335b09af708f4263ce61690ad8dbf259d3e4
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-10-19 11:49:20 -0700

    get it working with path

--- src/worker.ts
+++ src/worker.ts
@@ -13,10 +13,67 @@ import { sendEmail } from './email';
 
 type Env = Record<string, string>;
 
+const PLEX_PATH = '/plex';
+
+async function fetch(request: Request, env: Env) {
+	const method = request.method;
+
+	const requestPath = new URL(request.url).pathname;
+
+	// Handle the "plex" path
+	if (requestPath === PLEX_PATH) {
+		const apiKey = env.NOTION_INTEGRATION_KEY;
+		const databaseId = env.NOTION_PLEX_REQUEST_DATABASE_ID;
+		const correctPassword = env.PLEX_PASSWORD;
+
+		if (method === 'POST') {
+			const body = (await request.json()) as { title: string; why: string; who: string; password: string };
+			const title = body.title;
+			const why = body.why;
+			const who = body.who;
+			const password = body.password;
+
+			// Allow CORS request from localhost
+			const headers = new Headers(request.headers);
+
+			headers.set('Access-Control-Allow-Origin', 'http://localhost:8000');
+			headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
+			headers.set('Access-Control-Allow-Headers', 'Content-Type');
+			headers.set('Access-Control-Max-Age', '86400');
+
+			// Password check is case insensitive
+			if (password.toLowerCase() !== correctPassword.toLowerCase()) {
+				return new Response('Incorrect password', {
+					status: 401,
+					headers: headers,
+				});
+			}
+
+			await addPlexRequest(apiKey, databaseId, title, why, who);
+
+			await sendEmail(
+				env.SENDGRID_API_KEY,
+				env.SENDGRID_FROM_EMAIL,
+				env.SENDGRID_TO_EMAIL,
+				'New Plex Request from ' + who,
+				`<p>There is a new Plex request from ${who}.</p><p><strong>Title:</strong> ${title}</p><p><strong>Why:</strong> ${why}</p>`
+			);
+
+			const response = new Response('Record Added: ' + title, { status: 201, headers: headers });
+
+			return response;
+		} else {
+			return new Response('Method not allowed', { status: 405 });
+		}
+	} else {
+		return new Response('Not found', { status: 404 });
+	}
+}
+
 async function addPlexRequest(apiKey: string, databaseId: string, title: string, why: string, who: string) {
 	const notion = new Client({ auth: apiKey });
 
-	const response = await notion.pages.create({
+	await notion.pages.create({
 		parent: {
 			database_id: databaseId,
 			type: 'database_id',
@@ -54,47 +111,6 @@ async function addPlexRequest(apiKey: string, databaseId: string, title: string,
 			},
 		},
 	});
-}
-
-async function fetch(request: Request, env: Env) {
-	const method = request.method;
-	const apiKey = env.NOTION_INTEGRATION_KEY;
-	const databaseId = env.NOTION_PLEX_REQUEST_DATABASE_ID;
-	const correctPassword = env.PLEX_PASSWORD;
-
-	if (method === 'OPTIONS') {
-		return new Response(null, {
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-				'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-				'Access-Control-Allow-Headers': 'Content-Type',
-			},
-		});
-	} else if (method === 'POST') {
-		const body = (await request.json()) as { title: string; why: string; who: string; password: string };
-		const title = body.title;
-		const why = body.why;
-		const who = body.who;
-		const password = body.password;
-
-		if (password !== correctPassword) {
-			return new Response('Incorrect password', { status: 401 });
-		}
-
-		await addPlexRequest(apiKey, databaseId, title, why, who);
-
-		await sendEmail(
-			env.SENDGRID_API_KEY,
-			env.SENDGRID_FROM_EMAIL,
-			env.SENDGRID_TO_EMAIL,
-			'New Plex Request from ' + who,
-			`<p>There is a new Plex request from ${who}.</p><p><strong>Title:</strong> ${title}</p><p><strong>Why:</strong> ${why}</p>`
-		);
-
-		return new Response(`Record Added: ${title}`, { status: 200 });
-	} else {
-		return new Response('Method not allowed', { status: 405 });
-	}
 }
 
 export default {
